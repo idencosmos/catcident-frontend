0dbf961f53212b34eac86100080f47c514ebca3d
2kwonhee <id@senterie.com>
2025-03-05 18:49:31 +0900

    feat: Character 인터페이스에 creator 필드 및 books 필드 단순화

--- src/lib/api/_types/about/character.ts
+++ src/lib/api/_types/about/character.ts
@@ -5,9 +5,16 @@ import { Book } from './book';
 export interface Character {
   id: number;
   slug: string;
-  name: string;
   image: Media | null;
-  description: string; // HTML 문자열
-  books: Book[];
-  creator: string; // Creator 객체로 확장 가능
+  books: Book[] | null;
+  creator: {
+    id: number;
+    slug: string;
+    name: string;
+    photo: string | null;
+    bio_summary: string;
+    description: string;
+  } | string | null;
+  name: string;
+  description: string;
 }
